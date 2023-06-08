b84547044958587ed34b1f363ae128969eff8d82
Erkus00 <aragon1399@protonmail.com>
2023-06-08 10:39:01 +0200

    Aprendzaje basico AJAX y JQueryDONE

--- AjaxBasics/Node_AJAX_CRUD/src/public/javascript/DOM.js
+++ AjaxBasics/Node_AJAX_CRUD/src/public/javascript/DOM.js
@@ -6,30 +6,51 @@ $(document).ready(() => {
 	}).done(function (response) {
 		var html;
 		$.each(response, function (i, element) {
-			html = `<div class="item-box" data-id="${element.id}">
+			html = `<div><div class="item-box" data-id="${element.id} data-price="${element.price}">
 			<img src="../images/imagen.jpg" />
 			<div class="item-tittle">${element.name}</div>
-			<p>
+			<p class="item-description">
 				${element.description}
 			</p>
 			<div class="item-price">
 				<p>${element.price} - €</p>
 				<button>Add to cart</button>
 			</div>
-
 			<div><a href="#" class="info-link">MoreInfo</a></div>
 
 			<div class="more-info">
 				${element.moreInfo}
 			</div>
-		</div>`;
+		</div></div>`;
 
-			$("body").append(html);
+			$(".card-container").append(html);
 		});
+	});
 
-		$("body").on("click", ".info-link", function (e) {
-			e.preventDefault();
-			$(this).closest(".item-box").find(".more-info").slideToggle("slow");
+	$(".card-container").on("click", ".info-link", function (e) {
+		e.preventDefault();
+		$(this).closest(".item-box").find(".more-info").slideToggle("slow");
+	});
+
+	var total = 0;
+
+	$(".card-container").on("click", "div .item-box button", function (e) {
+		e.preventDefault();
+		// Queremos almacenar como un numero el Id; por lo que icluimos un "+"
+		var id = +$(this).closest(".item-box").data("id");
+		// var price = $(this).closest(".item-box").data("price");
+		// total += price;
+		// $(".price").text(total + " -€");
+
+		$.ajax("add.json", {
+			type: "post",
+			data: {
+				id: id,
+			},
+			dataType: "json",
+			contentType: "application/json",
+		}).done(function (response) {
+			console.log(response);
 		});
 	});
 });
